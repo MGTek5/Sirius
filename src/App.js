16c8bda18a51123577132e8cf2134fea033b1d41
Matteo Gassend <matteo.gassend@epitech.eu>
2021-10-19 16:13:03 +0200

    [add] userContext

--- src/App.js
+++ src/App.js
@@ -1,17 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { BrowserRouter } from "react-router-dom";
+import userContext from "./context/userContext";
 import "./index.css";
 import Router from "./Router";
+import { auth } from "./utils/firebase";
 
 function App() {
+  const [user, setUser] = useState();
+
+  useEffect(() => {
+    auth.onAuthStateChanged((change) => {
+      setUser(change);
+    });
+  });
+
   return (
     <>
-      <BrowserRouter>
-        <Router />
-      </BrowserRouter>
-      <footer>
-        <div className="bg-gray-400">footer</div>
-      </footer>
+      <userContext.Provider
+        value={{
+          user,
+          setUser: (u) => setUser(u),
+          logout: () => auth.signOut(),
+        }}
+      >
+        <BrowserRouter>
+          <Router />
+        </BrowserRouter>
+        <footer>
+          <div className="bg-gray-400">footer</div>
+        </footer>
+      </userContext.Provider>
     </>
   );
 }

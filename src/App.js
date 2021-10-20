40943ea2e790665ac63b5645bda46ba2302f8d05
Matteo Gassend <matteo.gassend@epitech.eu>
2021-10-20 09:42:20 +0200

    [add] themeContext for dark and light mode

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { BrowserRouter } from "react-router-dom";
+import themeContext from "./context/themeContext";
 import userContext from "./context/userContext";
 import "./index.css";
 import Router from "./Router";
@@ -7,6 +8,7 @@ import { auth } from "./utils/firebase";
 
 function App() {
   const [user, setUser] = useState();
+  const [theme, setTheme] = useState("light");
 
   useEffect(() => {
     auth.onAuthStateChanged((change) => {
@@ -15,7 +17,11 @@ function App() {
   });
 
   return (
-    <>
+    <div
+      className={`dark:text-white h-screen w-screen flex flex-col ${
+        theme === "dark" && "dark"
+      }`}
+    >
       <userContext.Provider
         value={{
           user,
@@ -23,14 +29,23 @@ function App() {
           logout: () => auth.signOut(),
         }}
       >
-        <BrowserRouter>
-          <Router />
-        </BrowserRouter>
-        <footer>
-          <div className="bg-gray-400">footer</div>
-        </footer>
+        <themeContext.Provider
+          value={{
+            theme,
+            changeTheme: () => {
+              setTheme(theme === "dark" ? "light" : "dark");
+            },
+          }}
+        >
+          <BrowserRouter>
+            <Router />
+          </BrowserRouter>
+          <footer>
+            <div className="bg-gray-400">footer</div>
+          </footer>
+        </themeContext.Provider>
       </userContext.Provider>
-    </>
+    </div>
   );
 }
 

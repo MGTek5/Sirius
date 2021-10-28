a288e11d22de67e2533eaa0fad6b1117b6b5a7f3
Matteo <matteo.gassend@epitech.eu>
2021-10-28 14:32:43 +0200

    [add] log out button on header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,18 @@
 import { useContext } from "react"
 import userContext from "../context/userContext"
+import Button from "./Button"
 
 const Header = () => {
 	const userC = useContext(userContext)
 
 	return (
-		<header>
-			<div className="w-full h-8 bg-gray-300 flex items-center">
+		<header className="">
+			<div className="w-full min-h-8 bg-gray-300 flex items-center justify-between px-8">
 				<h1 className="text-lg font-bold">Sirius</h1>
-				{userC.user !== null ? <div>
-					<button onClick={() => {
+				{userC.user !== null ? <div className="justify-self-end">
+					<Button bg="bg-blue-400" hoverBg="bg-blue-500" text="Sign Out" onClick={() => {
 						userC.logout()
-					}}>Log Out</button>
+					}} />
 				</div> : null}
 			</div>
 		</header>

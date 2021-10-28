4354a24840f46a76fc6d887d1e342d4091274ee7
Matteo <matteo.gassend@epitech.eu>
2021-10-28 14:53:51 +0200

    [add] login pages

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -7,10 +7,10 @@ import {BASE_URL} from "../utils/constants"
 import toast from "react-hot-toast";
 import queryString from 'query-string';
 import { useEffect } from "react";
-const Register = () => {
 
+const Login = () => {
 	const location = useLocation()
-    const history = useHistory()
+	const history = useHistory()
 	useEffect(() => {
 		const {auth} = queryString.parse(location.search)
 		if (auth) {
@@ -22,16 +22,15 @@ const Register = () => {
         initialValues: {
             email: "",
             password: "",
-            username:""
         },
         onSubmit: async (values) => {
             try {
-                await app.account.create(values.email, values.password, values.username)
-                toast.success("Account Created. You can now sign in")
-                history.push("/login")
+                await app.account.createSession(values.email, values.password, values.username)
+				toast.success("Welcome Back!")
+				history.push("/")
             } catch (error) {
                 console.log(error)
-                toast.error("Something went wrong while creating your account. Please check your credentials...")
+                toast.error("Something went wrong while signing into your account. Please check your credentials...")
             }
         }
     })
@@ -41,15 +40,12 @@ const Register = () => {
             <div className="bg-gray-200 dark:bg-gray-700 p-4 rounded-md shadow-lg h-96 flex flex-col justify-evenly">
                 <div className="mb-2">
                     <h2 className="text-center text-xl font-semibold">Welcome to Sirius</h2>
-                    <p>Sign up using the form below or using another service</p>
+                    <p>Sign in using the form below or using another service</p>
                 </div>
                 <div>
                     <form className="flex flex-col" onSubmit={formik.handleSubmit}>
                         <label htmlFor="email">Email</label>
                         <input onChange={formik.handleChange} name="email" value={formik.values.email} type="email" />
-                        
-                        <label htmlFor="username">Username</label>
-                        <input onChange={formik.handleChange} name="username" value={formik.values.username} />
                         <label htmlFor="password">Password</label>
                         <input onChange={formik.handleChange} name="password" value={formik.values.password} type="password" />
 
@@ -60,11 +56,11 @@ const Register = () => {
                     app.account.createOAuth2Session("google", `${BASE_URL}/?auth=success`, `${BASE_URL}/register?auth=failure`)
                 }} icon={<Google className="h-8 w-auto" />} />
                 <div className="text-right">
-                    <Link to="/login">Or sign in</Link>
+                    <Link to="/register">Or create an account</Link>
                 </div>
             </div>
         </main>
     )
 }
 
-export default Register;
+export default Login

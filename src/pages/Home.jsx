64d0a427c93d3ce92a4fcc1babb607e6faa998d6
Matteo Gassend <mgassend@gmail.com>
2021-11-04 14:23:05 +0100

    [add] simple marker on static position + listener for document updater

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,17 @@
 import mapbox from "mapbox-gl"
 import { useEffect, useRef } from "react"
 import 'mapbox-gl/dist/mapbox-gl.css';
+import {app} from "../utils/appwrite"
+import { POSITION_COLLECTION } from "../utils/constants";
 
 mapbox.accessToken = "pk.eyJ1IjoibWF0ZmlyZTE5OTkiLCJhIjoiY2t2anA4eGVmMDd0aDMzb3U1M2s0cWp6aSJ9.J6uwIZao8WEhN2An1pi76Q"
+
+
 
 const Home = () => {
 	const mapContainer = useRef(null);
 	const map = useRef()
+	const IssPosition = useRef(new mapbox.Marker())
 
 	useEffect(() => {
 		if (map.current) return;
@@ -18,12 +23,23 @@ const Home = () => {
 
 
 		})
+		IssPosition.current.setLngLat({lat: 46.56667, lon: 3.33333}).addTo(map.current)
 	})
 
+	useEffect(() => {
+		const unsub = app.subscribe(`collections.${POSITION_COLLECTION}.documents`, onDocumentsUpdate)
+		return unsub;
+	})
+
+	const onDocumentsUpdate = (payload) => {
+		console.log(payload)
+		if (payload.event === "database.documents.create") {
+		}
+	}
 
 	return (
 		<main>
-			<div ref={mapContainer} className="h-full w-full" />
+			<div className="h-full w-full" ref={mapContainer} />
 		</main>
 	)
 }

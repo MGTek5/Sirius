679ee1f8136f457016445c7340d0847071f9551c
Matteo <matteo.gassend@epitech.eu>
2021-11-11 18:29:37 +0100

    [fix] updating endpoint due to migrations

--- src/utils/appwrite.js
+++ src/utils/appwrite.js
@@ -5,7 +5,7 @@ const app = new Appwrite();
 
 app
     .setEndpoint('https://api.nirah.tech/v1') // Your Appwrite Endpoint
-    .setProject('61780ec6bc986') // Your project ID
+    .setProject('618d4b5b761f4') // Your project ID
 ;
 
 export {app};

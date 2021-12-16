3f8a6bb8f658e3c23a65d4e43765335b0751def6
Dythic <quentin.henry@epitech.eu>
2021-12-16 09:05:25 +0100

    [add] display map and checkpoint

--- src/pages/Location.tsx
+++ src/pages/Location.tsx
@@ -1,12 +1,48 @@
 import mapbox from "mapbox-gl";
-import { useEffect, useRef } from "react";
-import { MAPBOX_TOKEN } from "../utils/constants";
+import { useParams } from "react-router-dom";
+import userContext from "../context/userContext";
+import { useContext, useState, useEffect, useRef, } from "react";
+import { app, UserPosition } from "../utils/appwrite";
+import { useHistory } from "react-router";
+import LoadingScreen from "../components/LoadingScreen";
+import toast from "react-hot-toast";
+import {
+    MAPBOX_TOKEN,
+    USER_POSITION_COLLECTION
+} from "../utils/constants";
 
 const Location = () => {
+    const { idlocation } = useParams<{ idlocation: string }>();
+    const history = useHistory();
     const map = useRef<mapbox.Map>();
-    const marker = useRef<HTMLDivElement>(null);
+    const userC = useContext(userContext);
     const mapContainer = useRef<HTMLDivElement>(null);
+    const [loading, setLoading] = useState(true);
     mapbox.accessToken = MAPBOX_TOKEN;
+
+    useEffect(() => {
+        const getPositions = async () => {
+            try {
+                const positions = await app.database.getDocument<UserPosition>(USER_POSITION_COLLECTION, idlocation)
+                console.log(positions);
+                if (map.current !== undefined) {
+                    new mapbox.Marker().setLngLat({ lat: positions.latitude, lon: positions.longitude }).addTo(map.current);
+                    map.current?.setCenter({
+                        lon: positions.longitude,
+                        lat: positions.latitude,
+                    });
+                }
+                setLoading(false);
+            } catch (error) {
+                console.error(error);
+                toast.error(
+                    "Error position"
+                );
+                setLoading(false);
+            }
+        }
+        if (userC.user) getPositions()
+    }, [userC, map])
 
     useEffect(() => {
         if (map.current) return;
@@ -22,14 +58,28 @@ const Location = () => {
 
     return (
         <div className="w-full h-full flex flex-col md:flex-row">
+            {loading && (
+                <LoadingScreen text="Fetching data for selected time, please wait..." />
+            )}
             <div className="w-full md:w-2/3 h-96 md:h-full">
-                <div
-                    ref={marker}
-                    className="h-12 w-12 bg-cover"
-                ></div>
                 <div className="h-full w-full" ref={mapContainer} />
             </div>
             <div className="flex flex-col justify-evenly">
+                <div className="text-center">
+                    <div className="w-full px-4">
+                        <button
+                            onClick={
+                                async () => {
+                                    await app.database.deleteDocument(USER_POSITION_COLLECTION, idlocation);
+                                    history.push('/');
+                                    toast.success("Delete position");
+                                }}
+                            className={`btn btn-primary btn-block`}
+                        >
+                            Delete Position
+                        </button>
+                    </div>
+                </div>
             </div>
         </div>
     );
